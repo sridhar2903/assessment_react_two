124449bb257ba264161e9ab9b7ea780d2bb3d0a2
sridhar2903 <saisridhard@gmail.com>
2022-02-27 14:24:09 +0530

    changes

--- src/Assesment-two/Maincomponent.js
+++ src/Assesment-two/Maincomponent.js
@@ -1,63 +1,66 @@
 import React from 'react'
 import './style.css'
 
-class Maincomponent extends React.Component{
-    state={
-        name:"",
-        department:"",
-        rating:"",
-        empdetails:[]
+class Maincomponent extends React.Component {
+    state = {
+        name: "",
+        department: "",
+        rating: "",
+        empdetails: []
     }
-    updateinput=(event)=>
-    {
+    updateinput = (event) => {
         this.setState({
-            [event.target.name]:event.target.value
+            [event.target.name]: event.target.value
         })
     }
-    onsubmit=(event)=>{
+    onsubmit = (event) => {
         event.preventDefault()
 
-        const Objtemp={
-            name:this.state.name,
-            department:this.state.department,
-            rating:this.state.rating
-           
+        const Objtemp = {
+            name: this.state.name,
+            department: this.state.department,
+            rating: this.state.rating
+
         }
 
-        const arraytemp=this.state.empdetails;
+        const arraytemp = this.state.empdetails;
         arraytemp.push(Objtemp);
-        this.setState({empdetails:arraytemp});
+        this.setState({ empdetails: arraytemp });
     }
 
-    render()
-    {
+    render() {
         return (
             <>
-             <h1 id="headstyle">EMPLOYEE FEEDBACK FORM</h1>
-             <form id='emp' onSubmit={this.onsubmit}>
-                 <label for="name" id="namestyle">Name:</label>
-             <input type="text" id='name' name='name' onChange={this.updateinput}  placeholder=''value={this.state.name} required/><br></br><br></br>
-             <label for="dept" id="deptstyle">Department:</label>
-             <input type="text" id='dept' name='department' onChange={this.updateinput} placeholder='' value={this.state.department} required/><br></br><br></br>
-             <label for="rating" id="ratestyle">Rating:</label>
-             <input type="number" id='rating' name='rating' onChange={this.updateinput} placeholder='' value={this.state.rating} required/><br></br><br></br>
-             <button id="sub" >Submit</button><br></br><br></br>
+                 <div id="center">
+                <h1 id="headstyle">EMPLOYEE FEEDBACK FORM</h1>
+                </div><br/>
+                <div id="containerform">
+                <form  onSubmit={this.onsubmit}>
+                <br/><br/>
+                    &nbsp;&nbsp; <label for="name" id="namestyle">Name</label><br/><br/>
+                    &nbsp;&nbsp;<input type="text" id='name' name='name' onChange={this.updateinput} placeholder='Enter Name ' value={this.state.name} required /><br/><br/>
+                    &nbsp;&nbsp;<label for="dept" id="deptstyle">Department</label><br/><br/>
+                    &nbsp;&nbsp;<input type="text" id='dept' name='department' onChange={this.updateinput} placeholder='Enter Department' value={this.state.department} required /><br/><br/>
+                    &nbsp;&nbsp;<label for="rating" id="ratestyle">Rating</label><br/><br/>
+                    &nbsp;&nbsp;<input type="number" id='rating' name='rating' onChange={this.updateinput} placeholder='Enter Rating' value={this.state.rating} required /><br/><br/>
+                    &nbsp;&nbsp;<button id="sub" type="submit" >Submit</button><br/><br/>
+                   
+                </form>
+                </div>
+                <div id="flex">
+                    {this.state.empdetails.map((value, index) => {
 
-             </form>
-          <div id="flex">
-             {this.state.empdetails.map((value,index)=>{
-                
-                 return(
-                    
-                    <p id="details">
-                       Name: {value.name} || Department:{value.department} || Rating:{value.rating}
-                    </ p>
-                    
-                 )
-                 
-             })}
-             </div>
-           
+                        return (
+
+                            <p id="details">
+                                Name: {value.name} || Department:{value.department} || Rating:{value.rating}
+                            </ p>
+
+                        )
+
+                    })}
+                </div>
+
             </>
         )
     }
